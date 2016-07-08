1dcfcc18e23993890640dda5342a52a3da05f716
Brian An <brian.an@perkinelmer.com>
2016-07-08 09:52:48 +0800

    Create project

---
+++ DailyScrum/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace DailyScrum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new System.Windows.Forms.ListViewItem.ListViewSubItem[] {
+            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "1"),
+            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "Brian An"),
+            new System.Windows.Forms.ListViewItem.ListViewSubItem(null, "00:00:00", System.Drawing.Color.Red, System.Drawing.SystemColors.Window, new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0))))}, -1);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ColumnNo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnTimeSpan = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.labelTimeSpan = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ColumnNo,
+            this.ColumnName,
+            this.ColumnTimeSpan});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(230, 235);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.MouseEnter += new System.EventHandler(this.listView1_MouseEnter);
+            this.listView1.MouseLeave += new System.EventHandler(this.listView1_MouseLeave);
+            // 
+            // ColumnNo
+            // 
+            this.ColumnNo.Text = "No";
+            this.ColumnNo.Width = 30;
+            // 
+            // ColumnName
+            // 
+            this.ColumnName.Text = "Name";
+            this.ColumnName.Width = 105;
+            // 
+            // ColumnTimeSpan
+            // 
+            this.ColumnTimeSpan.Text = "Time span";
+            this.ColumnTimeSpan.Width = 90;
+            // 
+            // labelTimeSpan
+            // 
+            this.labelTimeSpan.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelTimeSpan.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.labelTimeSpan.Location = new System.Drawing.Point(0, 207);
+            this.labelTimeSpan.Name = "labelTimeSpan";
+            this.labelTimeSpan.Size = new System.Drawing.Size(230, 23);
+            this.labelTimeSpan.TabIndex = 1;
+            this.labelTimeSpan.Text = "00:00:00";
+            this.labelTimeSpan.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelTimeSpan.Click += new System.EventHandler(this.labelTotalTimeSpan_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 230);
+            this.Controls.Add(this.labelTimeSpan);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "DailyScrum";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader ColumnNo;
+        private System.Windows.Forms.ColumnHeader ColumnName;
+        private System.Windows.Forms.ColumnHeader ColumnTimeSpan;
+        private System.Windows.Forms.Label labelTimeSpan;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ DailyScrum/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DailyScrum
+{
+    public partial class Form1 : Form
+    {
+        private bool inMeeting;
+        private bool isOvertime;
+
+        private TimeSpan meetingTimeSpan;
+        private TimeSpan individualTimeSpan;
+        private TimeSpan overtimeSpan;
+        private int currentItemIndex;
+
+        private List<string> attendee;
+        private const string timeZero = "00:00:00";
+        private readonly TimeSpan standardDailyScrumMeetingTimeSpan = TimeSpan.FromMinutes(15);
+        private readonly TimeSpan standardIndividualSpeakingTimeSpan = TimeSpan.FromMinutes(2);
+        private readonly TimeSpan standardTotalMeetingTimeSpan = TimeSpan.FromMinutes(30);
+        private readonly Color startBackColor;
+        private readonly Color endBackColor = Color.Yellow;
+        private readonly byte colorSpanR;
+        private readonly byte colorSpanG;
+        private readonly byte colorSpanB;
+
+        private TimeSpan totalTimeSpan
+        {
+            get
+            {
+                return meetingTimeSpan + overtimeSpan;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            startBackColor = this.BackColor;
+            currentItemIndex = -1;
+
+            colorSpanR = Convert.ToByte(Math.Abs(endBackColor.R - startBackColor.R));
+            colorSpanG = Convert.ToByte(Math.Abs(endBackColor.G - startBackColor.G));
+            colorSpanB = Convert.ToByte(Math.Abs(endBackColor.B - startBackColor.B));
+
+            meetingTimeSpan = new TimeSpan();
+
+            inMeeting = false;
+            isOvertime = false;
+
+            InitalAttendeeList();
+            FillListView();
+        }
+
+        private void InitalAttendeeList()
+        {
+            attendee = new List<string>();
+            attendee.Add("Brian An");
+            attendee.Add("Plum Mei");
+            attendee.Add("Jim Ruan");
+            attendee.Add("Hensher Han");
+            attendee.Add("Sophie Wang");
+            attendee.Add("Ivy Wang");
+            attendee.Add("Zoe Zhao");
+            attendee.Add("Alan Wen");
+        }
+
+        private void FillListView()
+        {
+            this.listView1.Items.RemoveAt(0);
+            int no = 1;
+
+            string name = GetNextAttendee();
+            for (; name != null; name = GetNextAttendee())
+            {
+                var item = new ListViewItem(new string[] { no++.ToString(), name, timeZero }, no);
+                this.listView1.Items.Add(item);
+            }
+        }
+
+        private string GetNextAttendee()
+        {
+            if (attendee == null || attendee.Count == 0)
+            {
+                return null;
+            }
+
+            Random r = new Random();
+            int n = r.Next();
+            int i = n % attendee.Count;
+            string ret = attendee[i];
+            attendee.RemoveAt(i);
+            return ret;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!this.inMeeting)
+            {
+                this.inMeeting = true;
+                this.timer.Start();
+            }
+
+            if (this.listView1.SelectedItems.Count > 0)
+            {
+                if (this.currentItemIndex >= 0)
+                {
+                    this.listView1.Items[this.currentItemIndex].ForeColor = Color.DarkGray;
+                }
+
+                individualTimeSpan = new TimeSpan();
+                this.listView1.SelectedItems[0].ForeColor = Color.Blue;
+                this.currentItemIndex = this.listView1.SelectedItems[0].Index;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (this.inMeeting)
+            {
+                meetingTimeSpan = meetingTimeSpan.Add(TimeSpan.FromSeconds(1));
+                this.labelTimeSpan.Text = meetingTimeSpan.ToString();
+
+                individualTimeSpan = individualTimeSpan.Add(TimeSpan.FromSeconds(1));
+                this.listView1.SelectedItems[0].SubItems[2].Text = individualTimeSpan.ToString();
+
+                if (individualTimeSpan > standardIndividualSpeakingTimeSpan)
+                {
+                    //this.listView1.SelectedItems[0].UseItemStyleForSubItems = false;
+                    this.listView1.SelectedItems[0].ForeColor = Color.Red;
+                }
+            }
+            else if (this.isOvertime)
+            {
+                overtimeSpan = overtimeSpan.Add(TimeSpan.FromSeconds(1));
+                string main = this.labelTimeSpan.Text.Substring(0, 8);
+                this.labelTimeSpan.Text = main + string.Format(" ({0})", overtimeSpan);
+            }
+
+            if (meetingTimeSpan > standardDailyScrumMeetingTimeSpan)
+            {
+                this.labelTimeSpan.ForeColor = Color.Red;
+            }
+
+            SetBackColor();
+        }
+
+        private void SetBackColor()
+        {
+            if (totalTimeSpan > standardTotalMeetingTimeSpan)
+            {
+                return;
+            }
+
+            double progress = totalTimeSpan.TotalSeconds / standardTotalMeetingTimeSpan.TotalSeconds;
+            int r = Convert.ToInt32(startBackColor.R + colorSpanR * progress);
+            int g = Convert.ToInt32(startBackColor.G + colorSpanG * progress);
+            int b = Convert.ToInt32(startBackColor.B - colorSpanB * progress);
+
+            this.labelTimeSpan.BackColor = Color.FromArgb(255, r, g, b);
+        }
+
+        private void labelTotalTimeSpan_Click(object sender, EventArgs e)
+        {
+            if (this.inMeeting)
+            {
+                this.inMeeting = false;
+
+                this.isOvertime = true;
+                overtimeSpan = new TimeSpan();
+            }
+            else if (!this.labelTimeSpan.Text.Equals(timeZero))
+            {
+                this.inMeeting = true;
+                this.isOvertime = false;
+
+                this.meetingTimeSpan = this.meetingTimeSpan.Add(this.overtimeSpan);
+            }
+        }
+
+        private void listView1_MouseEnter(object sender, EventArgs e)
+        {
+            //this.Opacity = 1.0;
+        }
+
+        private void listView1_MouseLeave(object sender, EventArgs e)
+        {
+            //this.Opacity = 0.75;
+        }
+    }
+}
